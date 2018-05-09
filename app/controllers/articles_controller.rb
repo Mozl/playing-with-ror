23f3dc5982da27dea3213c3d54a2b9d6e300c5ef
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-05-09 14:40:22 +0100

    added error showing if title shorter than 5 chars

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,15 +6,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
   
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
    
-    @article.save
+   if @article.save
     redirect_to @article
+   else
+    render 'new'
+   end
   end
    
   private
